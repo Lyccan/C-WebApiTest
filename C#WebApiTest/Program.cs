7e884af38c5f12c75df5d0f76bcb645ee76f379f
Lyccan <topchernobyl@gmail.com>
2025-12-17 18:47:35 -0300

    Adicionar arquivos de projeto.

---
+++ C#WebApiTest/Program.cs
@@ -0,0 +1,64 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Run(async (HttpContext context) =>
+{
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Path.StartsWithSegments("/"))
+        {
+            await context.Response.WriteAsync($"Método {context.Request.Method}{Environment.NewLine}");
+            await context.Response.WriteAsync($"a URL é: {context.Request.Path} {Environment.NewLine}");
+
+            var keys = context.Request.Headers.Keys;
+            await context.Response.WriteAsync($"Headers: {Environment.NewLine}");
+            foreach (var key in keys)
+            {
+                await context.Response.WriteAsync($"{key}: {context.Request.Headers[key]} {Environment.NewLine}");
+            }
+        }
+
+        if (context.Request.Path.StartsWithSegments("/carros"))
+        {
+            await context.Response.WriteAsync($"Lista de carros: {Environment.NewLine}");
+
+            var carros = PopulateCarro.GetCarros();
+
+            foreach (var carro in carros)
+            {
+                await context.Response.WriteAsync($"Marca: {carro.Marca}, Ano de Fabricação: {carro.AnoFabricacao} {Environment.NewLine}");
+            }
+        }
+    }
+});
+
+app.Run();
+
+public class Carro
+{
+    public int Id { get; set; }
+    public string Marca { get; set; }
+    public int AnoFabricacao { get; set; }
+
+    public Carro(int id, string marca, int anoFabricacao)
+    {
+        Id = id;
+        Marca = marca;
+        AnoFabricacao = anoFabricacao;
+    }
+}
+
+    static class PopulateCarro
+{
+    private static List<Carro> carros = new List<Carro>
+    {
+        new Carro(1, "FIAT", 1999),
+        new Carro(2, "Wolksvagen", 1945),
+        new Carro(3, "Renault", 2007)
+    };
+
+    public static List<Carro> GetCarros() => carros;
+}
+
+
+
